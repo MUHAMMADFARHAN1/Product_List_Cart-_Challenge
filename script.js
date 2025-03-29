eb096cd4af00c5dec7ed9dad9cbcd9511cf41fb4
Muhammad Farhan <farhan_75230@hotmail.com>
2025-03-29 15:19:38 +0100

    Json File sucessfully fetched

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let data;
+
+async function Getdata() {
+  let response = await fetch("./data.json");
+  data = await response.json();
+  return data;
+}
+
+data = Getdata();
+console.log(data);
+
 //to pass parameter we need to use concept of anonymous functions here
 
 // function changeDisplay(button) {
@@ -16,7 +27,7 @@
 //   Brownie: 0,
 //   Panna: 0,
 // };
-let quatity = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let quatity = [1, 0, 0, 0, 0, 0, 0, 0, 0];
 
 const change_button = (CartButton) => {
   for (let button of CartButton) {
@@ -33,7 +44,7 @@ const change_button = (CartButton) => {
     button.addEventListener("mouseout", () => {
       button.style.backgroundColor = "hsl(13, 31%, 94%)";
       button.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg"width="21"      height="20"      fill="none"      viewBox="0 0 21 20"    >      <g fill="#C73B0F" clip-path="url(#a)">        <path          d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"        />        <path          d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"        />      </g>      <defs>        <clipPath id="a">          <path fill="#fff" d="M.333 0h20v20h-20z" />        </clipPath>      </defs>    </svg>' +
+        '<svg xmlns="http://www.w3.org/2000/svg"width="21"      height="20"      fill="none"      viewBox="0 0 21 20" >      <g fill="#C73B0F" clip-path="url(#a)">        <path          d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"        />        <path          d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"        />      </g>      <defs>        <clipPath id="a">          <path fill="#fff" d="M.333 0h20v20h-20z" />        </clipPath>      </defs>    </svg>' +
         '<p      style="        display: inline;        position: relative;        bottom: 4px;        left: 3px;      "    >      Add to Cart    </p>';
       //   button.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg> + Add to Cart";
     });
