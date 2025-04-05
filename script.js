e1cb239ee029d2d74cd0f0ed6b9c10041495fda5
Muhammad Farhan <farhan_75230@hotmail.com>
2025-04-05 14:24:26 +0200

    Legacy Code has been eliminated!

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//Javascript will be used here in all three paradigms: Imperative, Event driven and functional with objects
-//////Fetch Operation//////////////
+//Javascript will be used here in all three paradigms: Imperative, Event driven and functional with objects.
+//Object will be passed implciitly and will also capture control staructure and states in the code.
+////////////////////////////////////////////////////////////////////////Fetch Operation////////////////////////////////////////////
+//This array will represent the global state of the items and is retrieved as JSON from the server
 let dataGlobal;
 
 const getData = async () => {
@@ -10,8 +12,8 @@ const getData = async () => {
 };
 
 ///////////////////////////////////////////////////////////////////////////////Variables/////////////////////////////////////////////////////////////////////
-
 ////The Selectors of the collage
+
 //The selector to hold cost of each item
 let cost;
 // the selector to hold the main heading
@@ -33,9 +35,10 @@ let Cart_Items = [];
 //This number represents the total nuber of items in the cart
 let total = 0;
 
-//This number is used to manage the tab states
+//This number is used to manage the template states
 let number;
 
+//Temporary variable to capture the number event on the buttons
 let quatity = 0;
 /////////////////////////////////////////////////////////////////////////////Event Loop//////////////////////////////////////////////////////////////////////
 
@@ -49,7 +52,8 @@ let quatity = 0;
   Cart_Array_Update();
   Cart_Number();
   Cart_Empty();
-  set_eventlisteners();
+  set_eventlistener_Click();
+  set_eventlistener_Mouseout();
 
   //Most of program logic will go here
 })();
@@ -63,19 +67,6 @@ const get_selectors = () => {
   images = document.querySelectorAll(".Food_Pics");
   Cart_Count = document.querySelectorAll("#Cart_Num");
   CartButtons = document.querySelectorAll(".Click_Button");
-};
-
-const set_eventlisteners = () => {
-  for (let index = 0; index < CartButtons.length; index++) {
-    //images[index].src = dataGlobal[index].image.desktop;
-    CartButtons[index].addEventListener("mouseover", () => {
-      CartButtons[index].style.backgroundColor = "red";
-      CartButtons[index].innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>' +
-        quatity[0] +
-        '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>';
-    });
-  }
 };
 
 //This function will update the number of items in the main cart
@@ -94,6 +85,33 @@ const Update_Cart = () => {};
 
 //This function will show the checkout screen on pressing confirm button
 const Check_out = () => {};
+
+/////////////////////////////////////////////Event Listeners on the Elements//////////////////////////////////////
+const set_eventlistener_Click = () => {
+  for (let index = 0; index < CartButtons.length; index++) {
+    //images[index].src = dataGlobal[index].image.desktop;
+    CartButtons[index].addEventListener("click", () => {
+      CartButtons[index].style.backgroundColor = "red";
+      CartButtons[index].innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>' +
+        quatity[0] +
+        '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>';
+    });
+  }
+};
+
+const set_eventlistener_Mouseout = () => {
+  for (let index = 0; index < CartButtons.length; index++) {
+    //images[index].src = dataGlobal[index].image.desktop;
+    CartButtons[index].addEventListener("mouseleave", () => {
+      CartButtons[index].style.backgroundColor = "hsl(13, 31%, 94%)";
+      CartButtons[index].innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg"width="21"      height="20"      fill="none"      viewBox="0 0 21 20" >      <g fill="#C73B0F" clip-path="url(#a)">        <path          d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"        />        <path          d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"        />      </g>      <defs>        <clipPath id="a">          <path fill="#fff" d="M.333 0h20v20h-20z" />        </clipPath>      </defs>    </svg>' +
+        '<p      style="        display: inline;        position: relative;        bottom: 4px;        left: 3px;      "    >      Add to Cart    </p>';
+      //   button.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg> + Add to Cart";
+    });
+  }
+};
 
 ///////////////////////////////////Cart Operations and State Management///////////////////////////////////////////////////////
 // This function will make the card division empty if number of items is zero
@@ -239,67 +257,3 @@ const Template_Confirmation = `<div style="display: flex; flex-direction: row; p
 </div>`;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const change_button = (CartButton) => {
-  for (let button of CartButton) {
-    // button.innerHTML = "Changed by query selector";
-    // button.addEventListener("click", changeDisplay(button));
-    button.addEventListener("mouseover", () => {
-      button.style.backgroundColor = "red";
-      button.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>' +
-        quatity[0] +
-        '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>';
-    });
-
-    button.addEventListener("mouseout", () => {
-      button.style.backgroundColor = "hsl(13, 31%, 94%)";
-      button.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg"width="21"      height="20"      fill="none"      viewBox="0 0 21 20" >      <g fill="#C73B0F" clip-path="url(#a)">        <path          d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"        />        <path          d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"        />      </g>      <defs>        <clipPath id="a">          <path fill="#fff" d="M.333 0h20v20h-20z" />        </clipPath>      </defs>    </svg>' +
-        '<p      style="        display: inline;        position: relative;        bottom: 4px;        left: 3px;      "    >      Add to Cart    </p>';
-      //   button.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg> + Add to Cart";
-    });
-  }
-};
-
-// let CartButton = document.querySelectorAll(".Click_Button");
-// change_button(CartButton);
-
-/*
- button.innerHTML = "
- <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
- ";
-<svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
- "p"
-        ).innerText = `Button has been clicked ${index} times`;
-<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
-*/
-
-/*
-Notes: 
-//to pass parameter we need to use concept of anonymous functions here
-
-*/
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//console.log(array[0]);
-
-// function changeDisplay(button) {
-//   //button.style.backgroundcolor = "red";
-//   button.style.backgroundColor = "green";
-//   button.innerHTML = "Check";
-// }
-// let quatity = {
-//   Waffle: 0,
-//   Brûlée: 0,
-//   Macaron: 0,
-//   Tiramisu: 0,
-//   Baklava: 0,
-//   Pie: 0,
-//   Cake: 0,
-//   Brownie: 0,
-//   Panna: 0,
-// };
-// let quatity = [1, 0, 0, 0, 0, 0, 0, 0, 0];
