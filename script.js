be0b02ce0c49b0ca629bf8b42c2e4880a04070c4
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-02 15:49:22 +0200

    states fixed!

--- script.js
+++ script.js
@@ -107,6 +107,7 @@ const Check_out = () => {};
 
 /////////////////////////////////////////////Event Listeners on the Elements/////////////////////////////////////////////////
 
+//We need to create two sytates for the button, one active and another inactive
 //The listener on cart button to change it
 const set_eventlistener_Click = () => {
   for (let index = 0; index < CartButtons.length; index++) {
@@ -122,11 +123,12 @@ const set_eventlistener_Click = () => {
         cart_state[index] = 1;
         cart_seq.push(index);
         Cart_Empty();
-      } else {
-        CartButtons[index].style.backgroundColor = "hsl(13, 31%, 94%)";
-        CartButtons[index].innerHTML =
-          '<svg          xmlns="http://www.w3.org/2000/svg"          width="21"          height="20"          fill="none"          viewBox="0 0 21 20"        >          <g fill="#C73B0F" clip-path="url(#a)">            <path              d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"            />            <path              d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"            />          </g>          <defs>            <clipPath id="a">              <path fill="#fff" d="M.333 0h20v20h-20z" />            </clipPath>          </defs>        </svg>        <p          style="            display: inline;            position: relative;            bottom: 4px;            left: 3px;          "        >          Add to Cart        </p>';
       }
+      // } else {
+      //   CartButtons[index].style.backgroundColor = "hsl(13, 31%, 94%)";
+      //   CartButtons[index].innerHTML =
+      //     '<svg          xmlns="http://www.w3.org/2000/svg"          width="21"          height="20"          fill="none"          viewBox="0 0 21 20"        >          <g fill="#C73B0F" clip-path="url(#a)">            <path              d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"            />            <path              d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"            />          </g>          <defs>            <clipPath id="a">              <path fill="#fff" d="M.333 0h20v20h-20z" />            </clipPath>          </defs>        </svg>        <p          style="            display: inline;            position: relative;            bottom: 4px;            left: 3px;          "        >          Add to Cart        </p>';
+      // }
     });
     console.log(cart_seq);
   }
