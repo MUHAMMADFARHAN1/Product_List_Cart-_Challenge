52f15a42975b97ea7edb4b639d6d372e13938b5c
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-02 13:42:30 +0200

    cart state adjusted!

--- script.js
+++ script.js
@@ -119,9 +119,9 @@ const set_eventlistener_Click = () => {
           quantity[0] +
           '<svg style = "margin-bottom: 1px" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg> </button>';
         //here an implicit argument needs to be send to increment the count
-        Cart_Empty();
-        cart_state[index] = 1;
         cart_seq.push(index);
+        cart_state[index] = 1;
+        Cart_Empty();
       }
     });
     console.log(cart_seq);
@@ -158,8 +158,8 @@ const Cart_Empty = () => {
 
   if (number < 9) {
     cart_children.insertAdjacentHTML("afterend", cost_tab);
-    number++;
     Cart_textupdated();
+    number++;
   }
 
   console.log(number);
